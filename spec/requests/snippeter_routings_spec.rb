623e07b97f76ac98aff4154b27ce9d9d565f9400
gruchanet <grucha.net@gmail.com>
2014-05-17 04:13:02 +0200

    add includes

--- spec/requests/snippeter_routings_spec.rb
+++ spec/requests/snippeter_routings_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "Snippeter" do
+
   describe "GET /" do
     it "sends response with status code 200" do
       get root_path
       response.status.should be(200)
     end
   end
+
 end
