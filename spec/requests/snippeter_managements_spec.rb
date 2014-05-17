82f726903b215d587c92ef20b7e719895a1b3219
gruchanet <grucha.net@gmail.com>
2014-05-17 21:30:02 +0200

    more readable scenario "empty snippets page"

--- spec/requests/snippeter_managements_spec.rb
+++ spec/requests/snippeter_managements_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature "Snippeter management" do
 
   scenario "User visits empty `recent snippets` page" do
-    visit '/snippets'
-    
-    expect(page).to have_content 'No snippets found.'
+    visit snippets_path
+
+    page.should have_content('No snippets found.')
   end
 end
