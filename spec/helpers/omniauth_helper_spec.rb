2f67f7ef710078cc4e95adb8b30e73144300e7e7
gruchanet <grucha.net@gmail.com>
2014-05-18 02:18:49 +0200

    fix omniauth helper

--- spec/helpers/omniauth_helper_spec.rb
+++ spec/helpers/omniauth_helper_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 # Authentication helper, to simulate github connection
 OmniAuth.config.test_mode = true
-omniauth_hash = { 'provider' => 'github',
-                  'uid' => '12345',
-                  'info' => {
-                      'name' => 'natasha',
-                      'email' => 'hi@natashatherobot.com',
-                      'nickname' => 'NatashaTheRobot'
-                  },
-                  'extra' => {'raw_info' =>
-                                  { 'location' => 'San Francisco',
-                                    'gravatar_id' => '123456789'
-                                  }
-                  }
+omniauth_hash = {
+    'provider' => 'github',
+    'uid' => '12345',
+    'info' => {
+        'name' => 'Chuck the Tester',
+        'email' => 'chuck@test.it',
+        'nickname' => 'chuck_tester'
+    },
+    'extra' => {
+        'raw_info' =>
+            {
+                'location' => 'Testerro',
+                'gravatar_id' => '500100900'
+            }
+    }
 }
 
 OmniAuth.config.add_mock(:github, omniauth_hash)
